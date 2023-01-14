52db9bacf32572b4c5f0a31ea339fbe12cd4ada0
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-01-14 18:02:04 +0200

    FilterField component

---
+++ src/components/FilterField/FilterField.jsx
@@ -0,0 +1,64 @@
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+const FilterField = () => {
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    // background: '#FFFFFF',
+
+    border: '1px solid #EAEAEA',
+    boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.05)',
+    borderRadius: '5px',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+
+    [theme.breakpoints.up('md')]: {
+      width: '75ch',
+    },
+    '& .MuiInputBase-input': {
+      display: 'block',
+      color: '#575757',
+      padding: theme.spacing(1.7, 2, 1.7, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      //   transition: theme.transitions.create('width'),
+    },
+  }));
+
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </Search>
+  );
+};
+export default FilterField;
