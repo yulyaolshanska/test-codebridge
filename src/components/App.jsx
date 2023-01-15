5b7b24e9443052785f7ea828111b34679268e3ef
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-01-15 19:18:50 +0200

    Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
+import { ThemeProvider } from '@mui/material/styles';
+// import { GlobalStyles } from '@mui/material';
+import { CssBaseline } from '@mui/material';
+import { theme } from 'styles/theme';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const ArticlePage = lazy(() => import('../pages/ArticlePage/ArticlePage'));
+const Home = lazy(() => import('../pages/HomePage/HomePage'));
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Suspense fallback={<div>...</div>}>
+        {/* <GlobalStyles /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article" element={<ArticlePage />} />
+          <Route path="*" element={<div>Nooooo</div>} />
+          {/* </Route> */}
+        </Routes>
+        <CssBaseline />
+      </Suspense>
+    </ThemeProvider>
+  );
 };
