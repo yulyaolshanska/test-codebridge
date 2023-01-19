43f5d3df2137e336cef11920eac571affa581ddb
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-01-20 01:17:40 +0200

    netlify

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="test-codebridge">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
